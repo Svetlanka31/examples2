dfd8436aa17728499cf263b45641a5909d20a7af
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-07 15:58:54 +0300

    added example03

---
+++ example03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Имеется одномерный массив array из n элементов, требуется найти элемент массива равный find
+int [] array = {13, 35, 65,  24, 86, 9, 65, 13, 4};
+
+int n = array.Length;
+int find = 65;
+
+int index = 0;
+
+while(index<n){
+if (array[index]==find){
+    Console.WriteLine(index);
+    break;
+}
+    index++;
+}
